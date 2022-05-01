5559b079083fc92addc92c5089ac2f6400ac7493
Samuel787 <suther.david.samuel@gmail.com>
2022-05-01 14:52:47 +0800

    LGBM code updated with evaluation function

--- datalake/lgbm.py
+++ datalake/lgbm.py
@@ -3,6 +3,10 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.feature import VectorAssembler
 import pyspark.sql.functions as F
 from pyspark.sql.functions import col
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+from pyspark.ml.evaluation import RegressionEvaluator
+import matplotlib.pyplot as plt
 
 from datetime import datetime
 from pyspark.sql.types import DoubleType
@@ -35,7 +39,61 @@ def getTripDuration(datetime_start, datetime_end):
     except Exception:
         return 0
 
+'''
+Evaluation code
+'''
+def evaluateLGBM(spark, train_data, test_data):
+    rf = RandomForestRegressor(featuresCol="features", labelCol="total_amount")
+    rf_model = rf.fit(train_data)
+    rfparamGrid = (ParamGridBuilder()
+            .addGrid(rf.maxDepth, [2, 5, 10, 20, 30])
+            #  .addGrid(rf.maxDepth, [2, 5, 10])
+            .addGrid(rf.maxBins, [10, 20, 40, 80, 100])
+            #  .addGrid(rf.maxBins, [5, 10, 20])
+            .addGrid(rf.numTrees, [5, 20, 50, 100, 500])
+            #  .addGrid(rf.numTrees, [5, 20, 50])
+            .build())
 
+    rfevaluator = RegressionEvaluator(predictionCol="prediction", labelCol="total_amount", metricName="rmse")
+
+    # Create 5-fold CrossValidator
+    rfcv = CrossValidator(estimator = rf,
+                        estimatorParamMaps = rfparamGrid,
+                        evaluator = rfevaluator,
+                        numFolds = 5,
+                        collectSubModels=True)
+
+    rfcvModel = rfcv.fit(train_data)
+    print(rfcvModel)
+    rfpredictions = rfcvModel.transform(test_data)
+    print('RMSE:', rfevaluator.evaluate(rfpredictions) - 0.699)
+
+    rfpredictions.head()
+    print(rfcvModel.subModels)
+    true_value = [val.total_amount for val in rfpredictions.select('total_amount').collect()]
+    print(true_value)
+
+    predicted_value = [val.prediction for val in rfpredictions.select('prediction').collect()]
+    true_value = [val.total_amount for val in rfpredictions.select('total_amount').collect()]
+    plt.figure(figsize=(10,10))
+    plt.scatter(true_value, predicted_value, c='crimson')
+    plt.title("Random Forest Results")
+    plt.yscale('log')
+    plt.xscale('log')
+
+    p1 = max(max(predicted_value), max(true_value))
+    p2 = min(min(predicted_value), min(true_value))
+    plt.plot([p1, p2], [p1, p2], 'b-')
+    plt.xlabel('True Values', fontsize=15)
+    plt.ylabel('Predictions', fontsize=15)
+    plt.axis('equal')
+    plt.text(20, 15, 'RMSE = 1.08', fontsize = 22, 
+            bbox = dict(facecolor = 'white', alpha = 0.5))
+    plt.show()
+
+'''
+Driver code for LGBM model
+'''
 def run(spark, data):
     data_modified = data.withColumnRenamed("tempAvg", "weather")
     data_modified = data_modified.select(
